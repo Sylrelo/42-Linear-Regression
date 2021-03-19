f104f8991be6e737fa373385df8d5bff1b277ef8
Sylvain LOPEZ <synlo@outlook.com>
2021-03-20 00:53:33 +0100

    Workaround webworker local. Clean code. Séparation fichiers

---
+++ src/charts.js
@@ -0,0 +1,73 @@
+const defaultLineScale = {
+	data: [
+		{ x: 1, y: 0 },
+		{ x: 0, y: 1 }
+	],
+	label: " ",
+	borderColor: "#ff000055",
+	fill: false
+}
+
+const initChartData = () => {
+	const ctx = document.getElementById("chartData").getContext("2d"),
+		ctxLine = document.getElementById("chartLine").getContext("2d"),
+		ctxPrecision = document.getElementById("chartPrecision").getContext("2d"),
+		ctxMSE = document.getElementById("chartMSE").getContext("2d")
+
+	const sharedOptions = {
+		options: {
+			responsive: false,
+			maintainAspectRatio: false,
+			responsiveAnimationDuration: 0,
+			legend: {
+				display: false
+			},
+			animation: {
+				duration: 0
+			},
+			hover: {
+				animationDuration: 0
+			},
+			scales: {
+				xAxes: [
+					{
+						type: "linear",
+						position: "bottom"
+					}
+				]
+			}
+		}
+	}
+
+	chartPrecision = new Chart(ctxPrecision, {
+		type: "line",
+		data: {
+			datasets: []
+		},
+		...sharedOptions
+	})
+
+	chartMSE = new Chart(ctxMSE, {
+		type: "line",
+		data: {
+			datasets: []
+		},
+		...sharedOptions
+	})
+
+	chartLine = new Chart(ctxLine, {
+		type: "line",
+		data: {
+			datasets: [defaultLineScale]
+		},
+		...sharedOptions
+	})
+
+	chartData = new Chart(ctx, {
+		type: "scatter",
+		data: {
+			datasets: [[], []]
+		},
+		...sharedOptions
+	})
+}
---
+++ src/worker_train.js
@@ -0,0 +1,76 @@
+const worker_train = () => {
+	const derive = (data, thetas, learning_rate) => {
+		let der = [0.0, 0.0]
+
+		for (const curr of data) {
+			const tmp = thetas[0] + thetas[1] * curr[0] - curr[1]
+			der[0] += tmp
+			der[1] += tmp * curr[0]
+		}
+		return [
+			thetas[0] - learning_rate * (1 / data.length) * der[0],
+			thetas[1] - learning_rate * (1 / data.length) * der[1]
+		]
+	}
+
+	const mean_squared_error = (data, thetas) => {
+		let mse = 0.0
+
+		for (const curr of data) {
+			const tmp = thetas[0] + thetas[1] * curr[0] - curr[1]
+			mse += tmp * tmp
+		}
+
+		return (1 / data.length) * mse
+	}
+
+	const train = data => {
+		const start = new Date().getTime()
+
+		let i,
+			thetas = [0.0, 0.0],
+			thetas_tmp = [0.0, 0.0],
+			diff = [0.0, 0.0],
+			mse = 0.0
+
+		for (i = 0; i < data.iterations; i++) {
+			thetas_tmp = derive(data.data, thetas, data.learning_rate)
+			diff = [Math.abs(thetas[0] - thetas_tmp[0]), Math.abs(thetas[1] - thetas_tmp[1])]
+
+			if ((diff[0] + diff[1]) * 0.5 < data.precision) {
+				thetas = thetas_tmp
+				break
+			}
+			mse = mean_squared_error(data.data, thetas)
+			thetas = thetas_tmp
+
+			self.postMessage({ action: "ITERATION", i, thetas, precision: diff, mse })
+		}
+		const time_taken = new Date().getTime() - start
+		self.postMessage({ action: "DONE", thetas, iteration: i, time_taken })
+	}
+
+	self.onmessage = function (e) {
+		const { action } = e.data
+		switch (action) {
+			case "START":
+				train(e.data)
+				break
+			default:
+				break
+		}
+	}
+	console.log("Worker is listening.")
+}
+
+try {
+	if (window != self) {
+		console.log("Service worker detected.")
+		worker_train()
+	}
+} catch (e) {
+	if (e?.toString()?.includes("window is not defined")) {
+		console.log("Service worker detected.")
+		worker_train()
+	}
+}
